1fe28f474f93f4f1bb2d41c1d304a45d41445264
goldmedal <liugs963@gmail.com>
2014-11-26 16:20:42 +0800

    matrix finished

--- IKDDhw4.py
+++ IKDDhw4.py
@@ -27,6 +27,88 @@ for root, dirs, files in os.walk("webpage_data_5"):
         findLink(file, num)
 
 odict = collections.OrderedDict(sorted(grap.items()))
+noEndPoint = list()
+deadEnd = list()
+
+# check dead end point
+
+for item in odict :
+    if odict[item] :
+        noEndPoint.append(odict[item])
+    else :
+        deadEnd.append(item)
+
+# check the point link to dead end and remove the dead end point
+
+noLinkToEnd = list()
+linkToEnd = list()
+
+for endPoint in deadEnd :
+    i = 0
+    for item in noEndPoint:
+        i += 1
+        if item.count(endPoint) == 0 :
+            noLinkToEnd.append(item)
+        else:
+            linkToEnd.append(i)
+
+# remote the point link to dead end
+
+finalList = list()
+
+for link2end in linkToEnd :
+    i = 0
+    for item in noLinkToEnd :
+        i += 1
+        newItem = list()   
+        for value in item :
+            if value != link2end :
+                 newItem.append(value)
+        finalList.append(newItem)         
+
+# count the rate 
+        
+matrix = list()
+key = 0
+length = len(finalList)
+exist = list()
+
+# re number 
+
+for item in finalList : 
+    for value in item:
+        if exist.count(value) == 0 :
+            exist.append(value)
+exist.sort()
+
+for item in finalList :
+    for value in item:
+        i = item.index(value)
+        item[i] = exist.index(value)       
+
+# generate the matrix
+  
+for point in finalList :
+    pointLen = len(point)
+    item = list()
+    i=0
+    while i < length:
+       item.append(0)
+       i += 1
+    for value in point :
+        item[value] = 1/pointLen
+        
+    matrix.append(item)
+    key += 1
+
+#
+
+
+
+
+
+            
+
 
 
 
